22eb951c71db097d018dd548f265c6cb648c71ad
anilcanbudak <anilcan.budak@metu.edu.tr>
2019-01-13 18:25:55 +0300

    Arduiono code for PWM generation is uploaded.

---
+++ Arduino/PWM.ino
@@ -0,0 +1,52 @@
+int potPin = A2; 
+int analog_val = 0;
+int PWM =6;
+float Duty=0;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(potPin,INPUT);
+  pinMode(PWM,OUTPUT);
+  setPwmFrequency(6, 8);
+}
+
+void setPwmFrequency(int pin, int divisor) 
+{
+  byte mode;
+  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 64: mode = 0x03; break;
+      case 256: mode = 0x04; break;
+      case 1024: mode = 0x05; break;
+      default: return;
+ }
+    if(pin == 5 || pin == 6) {
+      TCCR0B = TCCR0B & 0b11111000 | mode;
+    } else {
+      TCCR1B = TCCR1B & 0b11111000 | mode;
+    }
+  } else if(pin == 3 || pin == 11) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 32: mode = 0x03; break;
+      case 64: mode = 0x04; break;
+      case 128: mode = 0x05; break;
+      case 256: mode = 0x06; break;
+      case 1024: mode = 0x07; break;
+      default: return;
+    }
+    TCCR2B = TCCR2B & 0b11111000 | mode;
+  }
+}
+
+void loop() 
+{
+  analog_val = analogRead(potPin);    
+  Duty=(val/4.01568); 
+  analogWrite(PWM,Duty);  
+  Serial.println(Duty) ;    
+}
